4aab8da6615bffe0ef1b2890d2e3248ab1b1cd17
valerielui <valerielui@users.noreply.github.com>
2024-04-14 01:50:18 -0700

    hopefully the last of updates for a while :,)

--- modes.js
+++ modes.js
@@ -4,7 +4,7 @@
                     var elementColor = $('.subcolor').css('color');
                     // changing to light mode
                     // if in dark mode
-                    if (elementColor == 'rgb(255, 255, 255)') {
+                    if (elementColor == 'rgb(255, 250, 250)') {
                         $('.maincolor').css('background-color','#dbd6d3');
                         $('.subcolor').css('color','#1a1918');
                         $('.midcolor').css('background-color', '#c6beb9');
@@ -15,20 +15,22 @@
                         $('.sun').css('display','none');
                         $('.moon').css('display','block');
                         $('.project-desc').css('border-bottom-color', '#000000');
+                        $('.callout').css('border-left', '3px solid darkgray')
                         // $('.hero').css('background-color','#fefdfb');
                     } // if in light mode 
-                    else if (elementColor != 'rgb(255, 255, 255)') {
+                    else if (elementColor != 'rgb(255, 250, 250)') {
                         $('.maincolor').css('background-color','#1a1918');
-                        $('.subcolor').css('color','white');
+                        $('.subcolor').css('color','#fffafa');
                         $('.midcolor').css('background-color', '#0d0d0c');
-                        $('#nav3').css('color','white');
+                        $('#nav3').css('color','#fffafa');
                         $('a').css('color','white');
-                        $('#abt1').css('border','1px dashed white');
-                        $('.mode').css('color','white');
+                        $('#abt1').css('border','1px dashed #fffafa');
+                        $('.mode').css('color','#fffafa');
                         $('.sun').css('display','block');
                         $('.moon').css('display','none');
-                        $('.project-desc').css('border-bottom-color', '#ffffff');
+                        $('.project-desc').css('border-bottom-color', '#fffafa');
                         $('.panel').css('background-color','#1a1918');
+                        $('.callout').css('border-left', '3px solid lightgray')
 
                     };
                 });
